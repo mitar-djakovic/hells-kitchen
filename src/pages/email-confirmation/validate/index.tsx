cd12a8313986564f89b615221ea034985c400726
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2023-07-20 13:53:48 +0200

    Setup basic architecture

---
+++ src/pages/email-confirmation/validate/index.tsx
@@ -0,0 +1,3 @@
+export default function ValidateEmailConfirmation() {
+	return <div>EmailConfirmationValidate</div>;
+}
---
+++ src/pages/reset-password/request/index.tsx
@@ -0,0 +1,3 @@
+export default function RequestResetPassword() {
+	return <div>RequestResetPassword</div>;
+}
