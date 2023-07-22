2b00810860a5b36ddd45f6db673952231bda835c
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2023-07-22 02:26:52 +0200

    Minor cleanup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config)
-      console.log('on', on);
+	e2e: {
+		setupNodeEvents(on, config) {
+			// eslint-disable-next-line import/no-unresolved,@typescript-eslint/no-var-requires,global-require
+			require('@cypress/code-coverage/task')(on, config);
 
-      return config
-    },
-    watchForFileChanges: true,
-    specPattern: 'src/',
-  },
-  component: {
-    specPattern: 'src/',
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
+			return config;
+		},
+		watchForFileChanges: true,
+		specPattern: 'src/',
+	},
+	component: {
+		specPattern: 'src/',
+		devServer: {
+			framework: 'next',
+			bundler: 'webpack',
+		},
+	},
 });
