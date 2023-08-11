2e3e1964c868f2b8a37cd6b996c43e7df9a91fde
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2023-08-11 23:49:34 +0200

    App wraped with theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from '../config';
 
 import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<ThemeProvider theme={theme}>
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
