89b70d7a23a71e7fdedf265998d5cce919c5914b
Mitar Djakovic <mitar-djakovic2401993@hotmail.com>
2023-08-31 16:20:11 +0200

    App use roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+// eslint-disable-next-line import/no-unresolved
+import { Roboto } from 'next/font/google';
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from '../config';
 
 import '../styles/global.css';
 
+const roboto = Roboto({
+	weight: '400',
+	subsets: ['latin'],
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<ThemeProvider theme={theme}>
-			<Component {...pageProps} />
-		</ThemeProvider>
+		<main className={roboto.className}>
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</main>
 	);
 }
